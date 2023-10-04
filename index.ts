cc7071097c65e3a5dfcfbfca68a738a1d98f1328
guihteixeira013 <103337698+guihteixeira013@users.noreply.github.com>
2023-10-04 16:47:17 -0300

    criando função: par ou ímpar?

--- index.ts
+++ index.ts
@@ -1,5 +1,20 @@
 // Import stylesheets
 import './style.css';
 
-// Respeitando a mandinga 
-console.log('hello world!');
+// identificando o 'click' e imediatamente atribuindo uma função para o click
+document.getElementById("parimpar-button").addEventListener('click', () => {
+
+  // a variável 'num' irá receber o valor já convertido para Number do element HTML Input
+  let num: number = Number(
+    (<HTMLInputElement>document.getElementById("parimpar-input")).value
+  );
+
+  // teste de condição
+  if (num % 2 == 0) {
+    // adicionando o texto dentro da div "É PAR"
+    document.getElementById("parimpar-div").innerHTML = "É PAR";
+  } else {
+    // adicionando o texto dentro da div "É ÍMPAR"
+    document.getElementById("parimpar-div").innerHTML = "É ÍMPAR";
+  }
+});
